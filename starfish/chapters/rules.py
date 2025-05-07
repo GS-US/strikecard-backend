8071937949eafc3e2e8a96054ad34dfe1d5ce916
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-06 20:48:56 -0700

    working on admin, rules

--- starfish/chapters/rules.py
+++ starfish/chapters/rules.py
@@ -2,11 +2,13 @@ import rules
 
 # Predicates for Chapter Permissions
 
+
 @rules.predicate
 def is_chapter_facilitator(user, chapter):
     if not user.is_authenticated:
         return False
     return user.is_chapter_facilitator(chapter)
+
 
 @rules.predicate
 def is_chapter_assistant(user, chapter):
@@ -14,11 +16,13 @@ def is_chapter_assistant(user, chapter):
         return False
     return user.is_chapter_assistant(chapter)
 
+
 @rules.predicate
 def is_chapter_member(user, chapter):
     if not user.is_authenticated:
         return False
     return user.is_chapter_member(chapter)
+
 
 # Permissions for Chapters
 rules.add_perm('chapters.view_chapter', is_chapter_member)
@@ -26,11 +30,13 @@ rules.add_perm('chapters.change_chapter', is_chapter_facilitator)
 
 # Predicates for Contact Permissions
 
+
 @rules.predicate
 def can_access_contact(user, contact):
     if not user.is_authenticated:
         return False
     return user.is_chapter_member(contact.chapter)
+
 
 # Permissions for Contacts
 rules.add_perm('contacts.view_contact', can_access_contact)
---
+++ starfish/starfish/local_settings.template.py
@@ -0,0 +1,9 @@
+SECRET_KEY = ''
+
+TIME_ZONE = 'America/Los_Angeles'
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+CONTACT_HASH_SALT = ''
