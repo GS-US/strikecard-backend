fc823dbdfc97cdbd4433810d22bfed4bc69052ee
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-08 17:19:32 -0700

    added AffiliatedPledge

--- starfish/partners/admin.py
+++ starfish/partners/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from starfish.admin import SoftDeletableAdmin
 
-from .models import PartnerCampaign
+from .models import AffiliatedPledge, PartnerCampaign
 
 
 @admin.register(PartnerCampaign)
@@ -16,3 +16,11 @@ class PartnerCampaignAdmin(SoftDeletableAdmin):
     search_fields = ('name', 'email', 'url', 'legacy_source')
     list_filter = ('created', 'modified')
     readonly_fields = ('key_string', 'created', 'modified', 'last_used_at')
+
+
+@admin.register(AffiliatedPledge)
+class AffiliatedPledgeAdmin(admin.ModelAdmin):
+    list_display = ("organization_name", "count", "submitted_by_user", "created")
+    list_filter = ("created",)
+    search_fields = ("organization_name", "notes", "submitted_by_user__username")
+    readonly_fields = ("created",)
