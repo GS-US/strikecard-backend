e5b8daa83c8e52f0ce56a31061a950bcea861949
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-12 20:19:40 -0700

    style: Standardize string quotes in dev_setup.py file

--- starfish/starfish/management/commands/dev_setup.py
+++ starfish/starfish/management/commands/dev_setup.py
@@ -4,21 +4,20 @@ from django.utils import timezone
 from chapters.tests.factories import ChapterFactory
 
 class Command(BaseCommand):
-    help = "Setup default dev data"
+    help = 'Setup default dev data'
 
     def handle(self, *args, **kwargs):
         User = get_user_model()
 
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "admin")
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', 'admin')
 
-        # Create sample chapters using factories
         chapter_titles = [
-            "Oregon",
-            "New York",
-            "No California",
-            "So California",
-            "Northeastern States",
+            'Oregon',
+            'New York',
+            'Northern California',
+            'Southern California',
+            'Northeastern States',
         ]
 
         for title in chapter_titles:
