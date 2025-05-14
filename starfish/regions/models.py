2e2e003014378197f267713e9854326ecacc5bb4
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-13 20:24:12 -0700

    added regions app, zip code import

---
+++ starfish/regions/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class State(models.Model):
+    code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Zip(models.Model):
+    state = models.ForeignKey(State, on_delete=models.PROTECT)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.code
