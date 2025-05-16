7e9b4c850ea81f20cb902c9c878edd108df6ffd5
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-16 12:28:53 -0700

    feat: add list display and filter options to admin models

--- starfish/regions/admin.py
+++ starfish/regions/admin.py
@@ -5,13 +5,18 @@ from .models import State, Zip
 
 @admin.register(State)
 class StateAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name']
+    list_display_links = list_display
     search_fields = ['code', 'name']
 
 
 @admin.register(Zip)
 class ZipAdmin(admin.ModelAdmin):
+    list_display = ['state', 'code']
+    list_display_links = list_display
     search_fields = [
         'code',
         'state__code',
         'state__name',
     ]
+    list_filter = ['state']
