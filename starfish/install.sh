daf34b69c65fe9f87da1aefffaafe7fc305b5408
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-22 15:14:04 -0700

    all chapters now get a state

--- starfish/install.sh
+++ starfish/install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 set -e
+set -x
 
 python3 -m venv .venv
 source .venv/bin/activate
 
 pip install -r requirements.txt
 pip install -r requirements_dev.txt
+
+python manage.py flush --no-input
+
+python manage.py migrate
+
+python manage.py loaddata regions/fixtures/regions.json
+
+python manage.py create_state_chapters
