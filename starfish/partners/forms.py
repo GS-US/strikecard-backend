74330f994f76613009f42b12a606064b69864d5d
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-24 21:43:16 -0700

    .

--- starfish/partners/forms.py
+++ starfish/partners/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+
 from .models import PartnerCampaign
+
 
 class PartnerCampaignForm(forms.ModelForm):
     class Meta:
         model = PartnerCampaign
-        fields = ['name', 'email', 'url', 'notes']
-        widgets = {
-            'notes': forms.Textarea(attrs={'rows': 4}),
-        }
+        fields = [
+            'name',
+            'email',
+            'url',
+        ]
