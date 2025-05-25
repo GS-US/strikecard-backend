a65176dfcf260f0b66e390a405b4e757708f0f42
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-24 21:00:29 -0700

    much fancier fake email,

--- starfish/contacts/urls.py
+++ starfish/contacts/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import PendingContactCreateView, PendingContactDetailView, validate_contact
+from .views import (
+    PendingContactCreateView,
+    PendingContactDetailView,
+    SuccessView,
+    validate_contact,
+)
 
 urlpatterns = [
     path('signup/', PendingContactCreateView.as_view(), name='pending_contact_signup'),
     path('validate/<str:token>/', validate_contact, name='validate_contact'),
     path(
-        'success/',
-        TemplateView.as_view(template_name='contacts/validation_success.html'),
+        'success/<str:slug>/',
+        SuccessView.as_view(),
         name='validation_success',
     ),
     path(
