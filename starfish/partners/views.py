b3f60a492fff5592533c292f68020f3f52a38591
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-28 13:30:41 -0700

    feat: add partner campaign lookup views and URLs

--- starfish/partners/views.py
+++ starfish/partners/views.py
@@ -1,12 +1,18 @@
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
-from .forms import PartnerCampaignForm
+from django.views.generic import CreateView, DetailView, FormView
+from .forms import PartnerCampaignCreateForm, PartnerCampaignLookupForm
 from .models import PartnerCampaign
 
 class PartnerCampaignCreateView(CreateView):
     model = PartnerCampaign
-    form_class = PartnerCampaignForm
+    form_class = PartnerCampaignCreateForm
     template_name = 'partners/partnercampaign_form.html'
+
+    def get_context_data(self, form=None):
+        context = super().get_context_data(fom=form)
+        context['lookup_form'] = PartnerCampaignLookupForm()
+        return context
+
     def get_success_url(self):
         return reverse('partner_campaign_thanks', kwargs={'pk': self.object.pk})
 
@@ -17,3 +23,17 @@ class PartnerCampaignThanksView(DetailView):
     def form_valid(self, form):
         response = super().form_valid(form)
         return response
+
+
+class PartnerCampaignLookupView(DetailView):
+    model = PartnerCampaign
+    template_name = 'partners/partnercampaign_detail.html'
+
+    def form_valid(self, form):
+        partner_key = form.cleaned_data.get('partner_key')
+        email = form.cleaned_data.get('email')
+        context = self.get_context_data(form=form)
+        context['object'] = PartnerCampaign.objects.get(
+            key_string=partner_key, email=email
+        )
+        return self.render_to_response(context)
