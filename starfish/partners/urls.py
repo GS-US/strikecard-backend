5543f6b4bbb33d5584675b4a4c63a27ec0f68d63
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-28 13:30:41 -0700

    feat: add partner campaign lookup views and URLs

--- starfish/partners/urls.py
+++ starfish/partners/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import PartnerCampaignCreateView, PartnerCampaignThanksView
+from .views import PartnerCampaignCreateView, PartnerCampaignThanksView, PartnerCampaignLookupView
 
 urlpatterns = [
     path('create/', PartnerCampaignCreateView.as_view(), name='partner_campaign_create'),
     path('thanks/<int:pk>/', PartnerCampaignThanksView.as_view(), name='partner_campaign_thanks'),
+    path(
+        'lookup/',
+        PartnerCampaignLookupView.as_view(),
+        name='partner_campaign_lookup',
+    ),
+    path(
+        'lookup/<int:pk>/',
+        PartnerCampaignLookupView.as_view(),
+        name='partner_campaign_detail',
+    ),
 ]
