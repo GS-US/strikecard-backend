2596cc3ebbf244f76488f98340a102cb8e672d33
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-28 15:09:13 -0700

    contact export and import working

--- starfish/partners/urls.py
+++ starfish/partners/urls.py
@@ -1,14 +1,35 @@
 from django.urls import path
+
 from .views import (
+    PartnerCampaignContactExportView,
     PartnerCampaignCreateView,
-    PartnerCampaignThanksView,
+    PartnerCampaignDetailView,
     PartnerCampaignLookupFormView,
-    PartnerCampaignDetailView
+    PartnerCampaignThanksView,
 )
 
 urlpatterns = [
-    path('create/', PartnerCampaignCreateView.as_view(), name='partner_campaign_create'),
-    path('thanks/<int:pk>/', PartnerCampaignThanksView.as_view(), name='partner_campaign_thanks'),
-    path('lookup/', PartnerCampaignLookupFormView.as_view(), name='partner_campaign_lookup'),
-    path('detail/<int:pk>/', PartnerCampaignDetailView.as_view(), name='partner_campaign_detail'),
+    path(
+        'create/', PartnerCampaignCreateView.as_view(), name='partner_campaign_create'
+    ),
+    path(
+        'thanks/<int:pk>/',
+        PartnerCampaignThanksView.as_view(),
+        name='partner_campaign_thanks',
+    ),
+    path(
+        'lookup/',
+        PartnerCampaignLookupFormView.as_view(),
+        name='partner_campaign_lookup',
+    ),
+    path(
+        'detail/<int:pk>/',
+        PartnerCampaignDetailView.as_view(),
+        name='partner_campaign_detail',
+    ),
+    path(
+        'export/<int:pk>/',
+        PartnerCampaignContactExportView.as_view(),
+        name='partner_campaign_export',
+    ),
 ]
