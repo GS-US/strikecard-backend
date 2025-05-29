d65ea9f1cbbf5bbd01aca19051fb0b14762bb91a
Jeff Fein-Worton <jeff@fein-worton.com>
2025-05-28 21:51:02 -0700

    refactor: simplify routing configuration by removing commented code

--- starfish/starfish/routing.py
+++ starfish/starfish/routing.py
@@ -1,17 +1,10 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-# Import your consumers and define WebSocket URL patterns here
-# For example:
-# from . import consumers
-# websocket_urlpatterns = [ ... ]
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # "websocket": AuthMiddlewareStack(
-    #     URLRouter(
-    #         websocket_urlpatterns
-    #     )
-    # ),
+    "websocket":
+        URLRouter(
+        )
+    ),
 })
