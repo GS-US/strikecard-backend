2d0d838e7131232cc7f7190956aeec3422520b99
lux-u-tenebris <lux.u.tenebris@gmail.com>
2025-06-17 13:19:55 -0500

    add placeholder text to form fields

--- starfish/partners/forms.py
+++ starfish/partners/forms.py
@@ -12,6 +12,17 @@ class PartnerCampaignCreateForm(forms.ModelForm):
             'url',
         ]
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        name = self.fields["name"]
+        name.widget = forms.TextInput(attrs={"placeholder": "e.g., Jordan Doe"})
+
+        email = self.fields["email"]
+        email.widget = forms.EmailInput(attrs={"placeholder": "e.g., j.doe@abc.com"})
+
+        url = self.fields["url"]
+        url.widget = forms.URLInput(attrs={"placeholder": "e.g., https://www.jdoe-campaign.com"})
 
 class PartnerCampaignLookupForm(forms.Form):
     slug = forms.CharField()
