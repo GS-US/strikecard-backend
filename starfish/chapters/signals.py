3846e55d8c19e7431ea3f707b63064dd11500b8d
lux-u-tenebris <lux.u.tenebris@gmail.com>
2025-06-17 14:31:50 -0500

    black fixes

--- starfish/chapters/signals.py
+++ starfish/chapters/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import OfflineTotal
 
+
 @receiver([post_save, post_delete], sender=OfflineTotal)
 def update_chapter_total_on_offline_total_change(sender, instance, **kwargs):
     instance.chapter.update_total_contacts()
--- starfish/contacts/signals.py
+++ starfish/contacts/signals.py
@@ -2,10 +2,12 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Contact, ExpungedContact
 
+
 @receiver([post_save, post_delete], sender=Contact)
 def update_chapter_total_on_contact_change(sender, instance, **kwargs):
     if instance.chapter:
         instance.chapter.update_total_contacts()
+
 
 @receiver([post_save, post_delete], sender=ExpungedContact)
 def update_chapter_total_on_expunged_contact_change(sender, instance, **kwargs):
