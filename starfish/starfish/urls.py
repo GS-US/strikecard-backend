19fc962fd170cb384b322b477d261afbd479d221
Jeff Fein-Worton <jeff@fein-worton.com>
2025-06-23 18:41:33 -0700

    rest framework frame-workin' (sorry)

--- starfish/starfish/urls.py
+++ starfish/starfish/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings  # Imported settings
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import GetTotalsView
+
 urlpatterns = [
     path('', PendingContactCreateView.as_view(), name='index'),
     path('p/<str:p>/', PendingContactCreateView.as_view(), name='partner_signup'),
@@ -10,6 +12,7 @@ urlpatterns = [
     path('contacts/', include('contacts.urls')),
     path('chapters/', include('chapters.urls')),
     path('partners/', include('partners.urls')),
+    path('api/totals/', GetTotalsView.as_view(), name='get_the_totals'),
 ]
 
 if settings.DEBUG_TOOLBAR:
