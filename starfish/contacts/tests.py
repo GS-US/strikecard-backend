894e749b6971555bcc6bc0b7d45e4bdfd305e6c4
lux-u-tenebris <lux.u.tenebris@gmail.com>
2025-06-24 19:34:13 -0500

    dynamic project dir for testing works with both test_discovery and manage.py
    this doesn't feel right but nothing else is working

--- starfish/contacts/tests.py
+++ starfish/contacts/tests.py
@@ -1,16 +1,21 @@
+import os
+from pathlib import PurePath
+
 from contacts.forms import PendingContactForm
 from django.core.management import call_command
 from django.test import TestCase
 from regions.models import Zip
+
+start_dir = PurePath(os.getcwd())
+if start_dir.name != "starfish":
+    start_dir /= "starfish"
 
 
 # Create your tests here.
 class TestPendingContactForm(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
-        # call_command("makemigrations", verbosity=0)
-        # call_command("migrate", verbosity=0)
-        call_command("loaddata", "starfish/regions/fixtures/regions.json")
+        call_command("loaddata", start_dir / "regions/fixtures/regions.json")
 
     def test_phone(self):
         success_phones = ("202 202 2020",)
